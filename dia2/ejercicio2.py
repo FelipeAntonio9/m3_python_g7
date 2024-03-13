735053c672d806a16e6617ec8c7d76676437a8ea
FelipeAntonio9 <FAAC5454@hotmail.com>
2024-03-13 19:37:49 -0300

    avance

---
+++ dia2/ejercicio2.py
@@ -0,0 +1,33 @@
+
+
+
+
+
+
+#STRING
+nombre='isra'
+edad = "44"
+print('a123124 _-!."!!%&')
+print(edad)#imprimir el contenido de la variable
+print("tu edad es")
+
+#duplicacion
+print(type(edad))
+print(3*edad)#edad edad edad
+print(3*edad)#444444
+
+print(type(numero))#<class 'int'>
+print(3*numero)#300
+
+#metodo count() y len()
+print("21.384.060-6".count("."))
+print(len(21.384.060-6))
+print(len(123456789))
+
+#metodo upper()mayuscula y lower()minuscula
+print("KaKaRoTo".upper())#KAKAROTO
+print("KaKaRoTo".lower())#kakaroto
+print("KaKaRoTo ".title())#Kakaroto
+
+#join -> unir elementos separados
+print(", ".join(["a","b","c"]))
