58fbddc02d5167525433980a85abe0198a943b5a
FelipeAntonio9 <FAAC5454@hotmail.com>
2024-03-28 07:32:19 -0300

    dia 12 concluido

---
+++ dia11/ejercicio11.py
@@ -0,0 +1,23 @@
+#Crear diccionario
+diccionario = {}
+
+diccionario["nombre"] = "Felipe"
+diccionario["edad"] = 20
+diccionario["ciudad"] = "Temuco"
+
+print(diccionario)
+
+#agregar diccionario
+diccionario["país"] = "Chile"
+
+print(diccionario)
+
+#cambiar valor de diccionario
+diccionario["nombre"] = "Antonio"
+
+print(diccionario)
+
+#eliminar elemento del diccionario
+del diccionario["nombre"]
+
+print(diccionario)
