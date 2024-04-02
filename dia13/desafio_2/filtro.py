9a165a544beb84b47803becf6ac199f08c82c880
FelipeAntonio9 <FAAC5454@hotmail.com>
2024-04-02 02:23:59 -0300

    Filtro.py

--- dia13/desafio_2/filtro.py
+++ dia13/desafio_2/filtro.py
@@ -1,3 +1,5 @@
+from sys import argv
+
 precios = {'Notebook': 700000,
 'Teclado': 25000,
 'Mouse': 12000,
@@ -5,14 +7,24 @@ precios = {'Notebook': 700000,
 'Escritorio': 135000,
 'Tarjeta de Video': 1500000}
 
-def filtrar(diccionario, umbral):
-    filtro = {k:v for k,v in diccionario.items() if v > umbral}
-    return filtro
+umbral = int(argv[1])
 
-filtrado = filtrar(precios, 30000)
-
-resultado = ', '.join(filtrado)
-
-print("")
-
-print(f"Los productos mayores al umbral son: {resultado}")
+def filtrar (precios, umbral, mayor_que = True):
+    if mayor_que:
+        filtro = [producto for producto in precios if precios[producto] > umbral]
+        print(f"Los productos mayores al umbral son: {', '.join(filtro)}")
+    else:
+        filtro = [producto for producto in precios if precios[producto] < umbral]
+        print(f"Los productos menores al umbral son: {', '.join(filtro)}")
+    
+#python dia13/desafio_2/filtro.py 30000
+if len(argv) == 2:
+    filtrar(precios,umbral)
+else:
+    #python dia13/desafio_2/filtro.py 30000 menor/mayor
+    if argv[2].lower() == "mayor":
+        filtrar(precios,umbral)
+    elif argv[2].lower() == "menor":
+        filtrar(precios,umbral, False)
+    else:
+        print("Lo sentimos, no es una operación válida")
---
+++ dia14/ejemplo14.py
@@ -0,0 +1,13 @@
+def elevar(base, exponente):
+    """elevar un numero(base) a un exponente o el objetivo es elevar la base al exponente
+
+    Args:
+        base (float): numero base de la potencia
+        exponente (float): exponente de la potencia
+
+    Returns:
+        float: resultado
+    """
+    return base**exponente
+
+elevar(2,4)
